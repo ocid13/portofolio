63cd7ebc0b969d70d4eca13cfdaa05cfbe0dd4a3
Ocid <haidakhoirurrosyid@gmail.com>
2024-09-09 17:36:02 +0700

    finishing portfolioSection without link

--- src/components/PortfolioSection.jsx
+++ src/components/PortfolioSection.jsx
@@ -1,38 +1,41 @@
 import React from "react";
 import 'boxicons';
-import port1 from '../assets/image/portfolio1.png'
+import port1 from '../assets/image/portfolio1.png';
+import port2 from '../assets/image/portfolio2.png';
 
 const PortfolioSection = () => {
+  const projects = [
+    {
+      imgSrc: port1,
+      title: 'Lezatara',
+      description: 'Lezatara adalah sebuah platform daring (website) yang bertujuan untuk memperkenalkan, melestarikan, dan membagikan kelezatan masakan Nusantara.'
+    },
+    {
+      imgSrc: port2,
+      title: 'Bantu Si Liar',
+      description: 'Bantu Si Liar merupakan sebuah aplikasi berbasis website yang berfungsi sebagai sumber informasi sekaligus media untuk melaporkan apabila seseorang menemukan hewan liar yang tidak memiliki tempat tinggal, yang berkeliaran tanpa pemilik atau hewan peliharaan yang hilang',
+    },
+    {
+      imgSrc: port1,
+      title: 'Point of Sale',
+      description: 'Point of sale merupakan aplikasi yang digunakan dalam membantu pengolahan data-data bisnis seperti data pembelian, eceran, transaksi hutang, transaksi retur pembelian, hingga pelaporan transaksi yang dibutuhkan dalam pengambilan keputusan oleh para pebisnis',
+    },
+  ];
+
   return (
     <section className="portfolio" id="portfolio">
       <h2 className="heading">Latest <span>Project</span></h2>
       <div className="portfolio-container">
-        <div className="portfolio-box">
-          <img src={port1} alt="" />
-          <div className="portfolio-layer">
-            <h4>Lezatara</h4>
-            <p>Lezatara adalah sebuah platform daring (website) yang bertujuan untuk memperkenalkan, melestarikan, dan membagikan kelezatan masakan Nusantara. Aplikasi ini dirancang untuk memberikan informasi yang lengkap dan mudah diakses seputar resep-resep masakan Indonesia, termasuk bahan-bahan, langkah-langkah, dan informasi tambahan mengenai resep makanan di setiap regional wilayah indonesia, dalam website ini kami membagi ke 6 regional besar di wilayah indonesia diantaranya, Sumatera, Jawa, Bali Nusra, Kalimantan, Sulawesi, dan terakhir Papua Maluku.</p>
-            <a href="#"><i className="bx bx-link-external"></i></a>
+        {projects.map((project, index) => (
+          <div className="portfolio-box" key={index}>
+            <img src={project.imgSrc} alt={project.title} />
+            <div className="portfolio-layer">
+              <h4>{project.title}</h4>
+              <p>{project.description}</p>
+              <a href="#"><i className="bx bx-link-external"></i></a>
+            </div>
           </div>
-        </div>
-
-        <div className="portfolio-box">
-          <img src={port1} alt="" />
-          <div className="portfolio-layer">
-            <h4>Lezatara</h4>
-            <p>Lezatara adalah sebuah platform daring (website) yang bertujuan untuk memperkenalkan, melestarikan, dan membagikan kelezatan masakan Nusantara. Aplikasi ini dirancang untuk memberikan informasi yang lengkap dan mudah diakses seputar resep-resep masakan Indonesia, termasuk bahan-bahan, langkah-langkah, dan informasi tambahan mengenai resep makanan di setiap regional wilayah indonesia, dalam website ini kami membagi ke 6 regional besar di wilayah indonesia diantaranya, Sumatera, Jawa, Bali Nusra, Kalimantan, Sulawesi, dan terakhir Papua Maluku.</p>
-            <a href="#"><i className="bx bx-link-external"></i></a>
-          </div>
-        </div>
-
-        <div className="portfolio-box">
-          <img src={port1} alt="" />
-          <div className="portfolio-layer">
-            <h4>Lezatara</h4>
-            <p>Lezatara adalah sebuah platform daring (website) yang bertujuan untuk memperkenalkan, melestarikan, dan membagikan kelezatan masakan Nusantara. Aplikasi ini dirancang untuk memberikan informasi yang lengkap dan mudah diakses seputar resep-resep masakan Indonesia, termasuk bahan-bahan, langkah-langkah, dan informasi tambahan mengenai resep makanan di setiap regional wilayah indonesia, dalam website ini kami membagi ke 6 regional besar di wilayah indonesia diantaranya, Sumatera, Jawa, Bali Nusra, Kalimantan, Sulawesi, dan terakhir Papua Maluku.</p>
-            <a href="#"><i className="bx bx-link-external"></i></a>
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   )
